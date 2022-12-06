c88c9a558dc74c3b163125a5e1297de16235636c
Lex <106777728+Lexus-Banton@users.noreply.github.com>
2022-12-06 11:22:14 -0800

    post route working

--- sql/setup.sql
+++ sql/setup.sql
@@ -2,6 +2,7 @@
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS github_users CASCADE;
+DROP TABLE IF EXISTS posts;
 
 CREATE TABLE users (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -15,4 +16,11 @@ CREATE TABLE github_users (
   login TEXT NOT NULL,
   email TEXT,
   avatar TEXT
+);
+
+CREATE TABLE posts (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  user_id BIGINT ,
+  description VARCHAR,
+  FOREIGN KEY (user_id) REFERENCES github_users(id)
 );
